0dac0d7309eb6c82a88eef040d130d5b8c7db0bf
RichPeasant <44239042+RichPeasant@users.noreply.github.com>
2020-09-23 19:52:25 -0600

    Updates

---
+++ precache-manifest.6ed4e914c7cef0c73daddfb4d880ef23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa886c603179f7ae61baaac49beae6e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f357784dcce9f6b3fdb",
+    "url": "/static/css/main.3ef46158.chunk.css"
+  },
+  {
+    "revision": "6442f25cf8649591ef8c",
+    "url": "/static/js/2.032a17c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.032a17c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f357784dcce9f6b3fdb",
+    "url": "/static/js/main.defa7dec.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
